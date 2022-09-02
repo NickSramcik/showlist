53274cb3518ec7585b167d718529e7219d29b3ff
linds0202 <lindsayarainey@gmail.com>
2022-09-02 14:14:17 -0700

    added ability to search for tv in addition to movies & removed undefined people results

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch')
 const Movie = require('../models/Movie')
 
 module.exports = {
--- public/js/main.js
+++ public/js/main.js
@@ -105,47 +105,56 @@ async function recommendMovie(){
     }
 }
 
-
-function autocomplete(inp, arr) {
-    /*the autocomplete function takes two arguments,
+/*the autocomplete function takes two arguments,
     the text field element and an array of possible autocompleted values:*/
-    var currentFocus;
+function autocomplete(inp, arr) {    
     /*execute a function when someone writes in the text field:*/
     inp.addEventListener("input", function(e) {
-        var a, b, i, val = this.value;
+        var a, b, i, val = this.value
+        
         /*close any already open lists of autocompleted values*/
-        closeAllLists();
-        if (!val) { return false;}
-        currentFocus = -1;
+        closeAllLists()
+
+        if (!val) { return false }
+        
         /*create a DIV element that will contain the items (values):*/
-        a = document.createElement("DIV");
-        a.setAttribute("id", this.id + "autocomplete-list");
-        a.setAttribute("class", "autocomplete-items");
+        a = document.createElement("DIV")
+        a.setAttribute("id", this.id + "autocomplete-list")
+        a.setAttribute("class", "autocomplete-items")
+        
         /*append the DIV element as a child of the autocomplete container:*/
-        this.parentNode.appendChild(a);
+        this.parentNode.appendChild(a)
+        
         /*for each item in the array...*/
         for (i = 0; i < arr.length; i++) {
             /*create a DIV element for each matching element:*/
-            b = document.createElement("DIV");
-            /*set the innerHTML to the title of the movie*/
-            b.innerHTML = arr[i].title;
-            /*insert a input field that will hold the current array item's value:*/
-            b.innerHTML += "<input type='hidden' value='" + arr[i].title + "'>";
-            /*execute a function when someone clicks on the item value (DIV element):*/
+            b = document.createElement("DIV")
+            
+            /*set the innerHTML to the title of the movie or show & 
+            insert a input field that will hold the current array item's value*/
+            if (arr[i].media_type === 'movie') {
+                b.innerHTML = arr[i].title
+                b.innerHTML += "<input type='hidden' value='" + arr[i].title + "'>"
+            } else {
+                b.innerHTML = arr[i].original_name
+                b.innerHTML += "<input type='hidden' value='" + arr[i].original_name + "'>"
+            }
+            
+            /*execute a function when user clicks on the item value (DIV element):*/
             b.addEventListener("click", function(e) {
                 /*insert the value for the autocomplete text field:*/
-                inp.value = this.getElementsByTagName("input")[0].value;
+                inp.value = this.getElementsByTagName("input")[0].value
                 /*close the list of autocompleted values,
                 (or any other open lists of autocompleted values:*/
-                closeAllLists();
-            });
-            a.appendChild(b);
-          //}
+                closeAllLists()
+            })
+            a.appendChild(b)
         }
-    });
-    function closeAllLists(elmnt) {
-      /*close all autocomplete lists in the document,
+    })
+
+    /*close all autocomplete lists in the document,
       except the one passed as an argument:*/
+    function closeAllLists(elmnt) {
       var x = document.getElementsByClassName("autocomplete-items");
       for (var i = 0; i < x.length; i++) {
         if (elmnt != x[i] && elmnt != inp) {
@@ -153,23 +162,29 @@ function autocomplete(inp, arr) {
         }
       }
     }
+
     /*execute a function when someone clicks in the document:*/
     document.addEventListener("click", function (e) {
         closeAllLists(e.target);
-    });
+    })
 }
   
 //set up search by keyword        
 let runSearch = function () {
     let matches = []
     let keyword = this.value
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=8833c81e6715de20a62fb3e2ddc97c58&query=${keyword}`
+    
+    //if no text, don't search
+    if (keyword === '') return ''
+
+    let url = `https://api.themoviedb.org/3/search/multi?api_key=8833c81e6715de20a62fb3e2ddc97c58&query=${keyword}`
     fetch(url)
         .then(result => result.json())
         .then((data)=>{
             matches.push(...data.results)
-
-            autocomplete(document.getElementById("myInput"), matches)            
+            
+            //call the autocomplete function to populate the select dropdown
+            autocomplete(document.getElementById("myInput"), matches.filter(entry => entry.media_type !== 'person'))            
         })
         .catch(function(err){
             alert(err);
@@ -178,6 +193,7 @@ let runSearch = function () {
 
 const searchInput = document.querySelector('#myInput')
 
+//listen for user input in search box
 searchInput.addEventListener('change', runSearch)
 searchInput.addEventListener('keyup', runSearch)
 
