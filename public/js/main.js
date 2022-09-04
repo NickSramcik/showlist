301f66bff9346f71588a92c6fc97a77b8351d26e
questerson <yeadoneric@gmail.com>
2022-09-03 21:09:17 -0400

    added tabs in the page for watched list vs need to watch list

--- public/js/main.js
+++ public/js/main.js
@@ -32,6 +32,28 @@ Array.from(recommendBtn).forEach((el)=>{
 //     el.addEventListener('click', markUnWatched)
 // })
 
+
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+  })
+})
+
+
+
+
 async function deleteMovie(){
     const movieId = this.parentNode.dataset.id
     try{
