e61d871b328851f0b4cadbefaee510dcd802f827
shockleyje <8533782+ShockleyJE@users.noreply.github.com>
2022-09-12 18:35:39 -0400

    login screens styled

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,106 +1,132 @@
-const passport = require('passport')
-const validator = require('validator')
-const User = require('../models/User')
+const passport = require("passport");
+const validator = require("validator");
+const User = require("../models/User");
 
 // redirect to showlist if logged in, otherwise back to root
- exports.getLogin = (req, res) => {
-   // Check to see if user is logged in, if so, redirect to showlist page
-   if (req.user) {
-      return res.redirect('/movies')
-    }
-    res.render('login', {
-      title: 'Login'
-    })
+exports.getLogin = (req, res) => {
+  // Check to see if user is logged in, if so, redirect to showlist page
+  if (req.user) {
+    return res.redirect("/movies");
   }
-  
-  exports.postLogin = (req, res, next) => {
-    // initialize an array to contain the validation errors that occur
-    // validate each field, and if the check fails push the message 
-    const validationErrors = []
-    if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
-    if (validator.isEmpty(req.body.password)) validationErrors.push({ msg: 'Password cannot be blank.' })
-  
-    // Check if validation errors exist, if so, display them
-    if (validationErrors.length) {
-      req.flash('errors', validationErrors)
-      return res.redirect('/login')
-    }
-    // use the normalizeEmail function in the validator module to do something for the email?
-    req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
+  res.render("login", {
+    title: "Login",
+  });
+};
 
-    
-    passport.authenticate('local', (err, user, info) => {
-      if (err) { return next(err) }
-      // Check if user is NOT logged in. If so, flash error, redirect to login
-      if (!user) {
-        req.flash('errors', info)
-        return res.redirect('/login')
+exports.postLogin = (req, res, next) => {
+  // initialize an array to contain the validation errors that occur
+  // validate each field, and if the check fails push the message
+  const validationErrors = [];
+  if (!validator.isEmpty(req.body.firstName))
+    validationErrors.push({ msg: "Please enter a valid first name." });
+  if (!validator.isEmpty(req.body.lastName))
+    validationErrors.push({ msg: "Please enter a valid last name." });
+  if (!validator.isEmpty(req.body.username))
+    validationErrors.push({ msg: "Please enter a valid username." });
+  if (!validator.isEmail(req.body.email))
+    validationErrors.push({ msg: "Please enter a valid email address." });
+  if (validator.isEmpty(req.body.password))
+    validationErrors.push({ msg: "Password cannot be blank." });
+
+  // Check if validation errors exist, if so, display them
+  if (validationErrors.length) {
+    req.flash("errors", validationErrors);
+    return res.redirect("/login");
+  }
+  // use the normalizeEmail function in the validator module to do something for the email?
+  req.body.email = validator.normalizeEmail(req.body.email, {
+    gmail_remove_dots: false,
+  });
+
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    // Check if user is NOT logged in. If so, flash error, redirect to login
+    if (!user) {
+      req.flash("errors", info);
+      return res.redirect("/login");
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
       }
-      req.logIn(user, (err) => {
-        if (err) { return next(err) }
-        req.flash('success', { msg: 'Success! You are logged in.' })
-        res.redirect(req.session.returnTo || '/movies')
-      })
-    })(req, res, next)
-  }
+      req.flash("success", { msg: "Success! You are logged in." });
+      res.redirect(req.session.returnTo || "/movies");
+    });
+  })(req, res, next);
+};
 
+exports.logout = (req, res) => {
+  req.logout(() => {
+    console.log("User has logged out.");
+  });
+  // Destroy session & redirect to main page
+  req.session.destroy((err) => {
+    if (err)
+      console.log("Error : Failed to destroy the session during logout.", err);
+    req.user = null;
+    res.redirect("/");
+  });
+};
 
-  exports.logout = (req, res) => {
-    req.logout(() => {
-      console.log('User has logged out.')
-    })
-    // Destroy session & redirect to main page
-    req.session.destroy((err) => {
-      if (err) console.log('Error : Failed to destroy the session during logout.', err)
-      req.user = null
-      res.redirect('/')
-    })
+exports.getSignup = (req, res) => {
+  if (req.user) {
+    return res.redirect("/movies");
   }
-  
-  exports.getSignup = (req, res) => {
-    if (req.user) {
-      return res.redirect('/movies')
-    }
-    res.render('signup', {
-      title: 'Create Account'
-    })
+  res.render("signup", {
+    title: "Create Account",
+  });
+};
+
+exports.postSignup = (req, res, next) => {
+  const validationErrors = [];
+  if (!validator.isEmail(req.body.email))
+    validationErrors.push({ msg: "Please enter a valid email address." });
+  if (!validator.isLength(req.body.password, { min: 8 }))
+    validationErrors.push({
+      msg: "Password must be at least 8 characters long",
+    });
+  if (req.body.password !== req.body.confirmPassword)
+    validationErrors.push({ msg: "Passwords do not match" });
+
+  if (validationErrors.length) {
+    req.flash("errors", validationErrors);
+    return res.redirect("../signup");
   }
-  
-  exports.postSignup = (req, res, next) => {
-    const validationErrors = []
-    if (!validator.isEmail(req.body.email)) validationErrors.push({ msg: 'Please enter a valid email address.' })
-    if (!validator.isLength(req.body.password, { min: 8 })) validationErrors.push({ msg: 'Password must be at least 8 characters long' })
-    if (req.body.password !== req.body.confirmPassword) validationErrors.push({ msg: 'Passwords do not match' })
-  
-    if (validationErrors.length) {
-      req.flash('errors', validationErrors)
-      return res.redirect('../signup')
-    }
-    req.body.email = validator.normalizeEmail(req.body.email, { gmail_remove_dots: false })
-  
-    const user = new User({
-      userName: req.body.userName,
-      email: req.body.email,
-      password: req.body.password
-    })
-  
-    User.findOne({$or: [
-      {email: req.body.email},
-      {userName: req.body.userName}
-    ]}, (err, existingUser) => {
-      if (err) { return next(err) }
+  req.body.email = validator.normalizeEmail(req.body.email, {
+    gmail_remove_dots: false,
+  });
+
+  const user = new User({
+    userName: req.body.userName,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  User.findOne(
+    { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
+    (err, existingUser) => {
+      if (err) {
+        return next(err);
+      }
       if (existingUser) {
-        req.flash('errors', { msg: 'Account with that email address or username already exists.' })
-        return res.redirect('../signup')
+        req.flash("errors", {
+          msg: "Account with that email address or username already exists.",
+        });
+        return res.redirect("../signup");
       }
       user.save((err) => {
-        if (err) { return next(err) }
+        if (err) {
+          return next(err);
+        }
         req.logIn(user, (err) => {
           if (err) {
-            return next(err)
+            return next(err);
           }
-          res.redirect('/movies')
-        })
-      })
-    })
-  }
+          res.redirect("/movies");
+        });
+      });
+    }
+  );
+};
